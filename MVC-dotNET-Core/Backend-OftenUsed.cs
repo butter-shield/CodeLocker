d7424679ab0f6470ba286a7c09a24114ebdc6b28
Butters <butters.devs@gmail.com>
2025-01-23 23:11:31 -0600

    Update Backend-OftenUsed.cs

--- MVC-dotNET-Core/Backend-OftenUsed.cs
+++ MVC-dotNET-Core/Backend-OftenUsed.cs
@@ -3,3 +3,11 @@ foreach (var key in form.Keys)
 {
     Debug.WriteLine(key + ":" + form[key]);
 }
+
+//Need this in every controller
+private readonly AppDBContext _context;
+
+public <name>Controller(AppDBContext context)
+{
+    _context = context ?? throw new ArgumentNullException(nameof(context));
+}
